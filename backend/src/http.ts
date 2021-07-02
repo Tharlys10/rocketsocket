a9c7ace49295baf53f63f259782015bde7a9dfa7
Tharlys Alves <tharlys2015c@gmail.com>
2021-07-01 23:37:48 -0300

    feat: :sparkles: pre config and created connect mongodb

---
+++ backend/src/http.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+import mongoose from 'mongoose';
+
+const app = express();
+
+mongoose.connect("mongodb://localhost/rocketsocket", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const server = createServer(app);
+
+app.use(express.static(path.join(__dirname, '..', '..', 'frontend')));
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log("Socket", socket.id)
+});
+
+export { server, io }
